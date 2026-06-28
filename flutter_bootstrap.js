1f878be5482e3cd77d2ceaa42af6c4b07aad04ed
Carlos Vélez <80561943+carlsevels@users.noreply.github.com>
2026-06-27 21:09:16 -0600

    Built web
    Branch: main
    Commit: 3a25a84ff831e5aa75e6f896c168b23c0d9ec0c9
    
    package:peanut 6.0.1

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"a10d8ac38de835021c8d2f920dbf50a920ccc0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1367982215" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "935765226" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
